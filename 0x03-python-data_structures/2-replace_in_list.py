55cf3c255c52250d6bde5d3e05f5c807dad9bbc2
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-08 16:53:58 +0300

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
+        # Return the original list if the index is out of bonds.
         return my_list
     my_list[idx] = element
-    return my_list.append(element)
+    return my_list
