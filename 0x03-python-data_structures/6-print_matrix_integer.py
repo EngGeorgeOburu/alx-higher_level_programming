777131ef812af50cda17f62212d53952703e2bb3
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-08 18:38:51 +0300

    fixing matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         for i, num in enumerate(row):
             # Check if it's the last element in the row
             if i != len(row) - 1:
-                print("{:d}".format(num), end="")
+                # Adds a space after each number
+                print("{:d}".format(num), end=" ")
             else:
                 print("{:d}".format(num), end="")
-    print()
+        print()  # Move to the next line after a complete row
