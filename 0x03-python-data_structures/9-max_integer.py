85d89799fe6ec804d10b1112c4fc0c9d4cfc461a
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-08 10:14:01 +0300

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    # Check if the list os empty
+    if not my_list:
+        return (0,None)
+   # Initialize the max num with the first element
+    max_num = my_list[0]
+    # Iterate through the list finding max num
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+    return max_num
