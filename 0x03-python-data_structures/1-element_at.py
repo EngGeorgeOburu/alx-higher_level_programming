f63bafecfb9418c493b79120692d47c49862b2da
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-08 16:39:00 +0300

    fixing pep

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-     """ Checks if the idx is negative or greater than 
-     the no. of elements in the list provided."""
+    # Check validity of idx
     if idx < 0 or idx > len(my_list):
         return None
     return my_list[idx]
