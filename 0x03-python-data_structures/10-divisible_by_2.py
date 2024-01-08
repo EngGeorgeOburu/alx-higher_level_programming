be81dd37d0c3527f48f6261c9d5a1d6a3d877931
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-08 11:23:16 +0300

    multiple of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    # Accessing each element in the list
+    for i in range (my_list):
+        if my_list[i] % 2 == 0:
+            return True
+        else:
+            return False
