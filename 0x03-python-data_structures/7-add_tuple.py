3dfa4442217203dd72acec8d535462d1d2d7d836
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-08 19:45:49 +0300

    adding tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    # Check if the length of tuple_a is >= 2
+    if len(tuple_a) >= 2:
+        a1, a2 = tuple_a[:2]  # Extract the first two elements
+    else:
+        a1, a2 = 0, 0  # Default values if the length is < 2
+    # Check if the lenghth of tuple_b is >= 2
+    if len(tuple_b) >= 2:
+        b1, b2 = tuple_b[:2]  # Extract the first two elements
+    else:
+        b1, b2 = 0, 0  # Default values if the length
+    # Return the results of the corresponding elements
+    return (a1 + b1, a2 + b2)
