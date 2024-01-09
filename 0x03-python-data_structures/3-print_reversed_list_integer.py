631ae3e7ca5ab84a5045880d59e5a36b6c141bd5
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-09 23:17:32 +0300

    3-print

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@ def print_reversed_list_integer(my_list=[]):
         # Create a new list if Nothing is provided
         my_list = []
     for num in reversed(my_list):
-        print("{}".format(num))
+        print("{:d}".format(num))
