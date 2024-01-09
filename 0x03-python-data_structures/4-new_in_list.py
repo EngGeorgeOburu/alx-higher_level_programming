c1a8f5ae70788aac78ba3e0acfb60f7cc04c0399
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-09 23:20:34 +0300

    fixing 4-new

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     # Check if idx is out of range or negative
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         # [:] is used to indicate start,end and step indices
         return my_list[:]  # Return a copy of the new element
     new_list = my_list[:]  # Creating a new list using list slicing
