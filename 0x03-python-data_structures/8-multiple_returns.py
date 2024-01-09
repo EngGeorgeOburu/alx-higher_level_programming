b5b2945ceda0d4654b3c4172a90b64bcf9d88a71
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-09 23:59:51 +0300

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     # Check if the sentence is empty
     if not sentence:
-        return None
+        return (0, None)
     else:
         # Return string length and first character
         return len((sentence), sentence[0])
