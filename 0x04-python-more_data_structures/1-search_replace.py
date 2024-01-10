6e83824b583ae34aadd6f94053ba41f062679aa5
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-10 22:36:19 +0300

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhon3
+def search_replace(my_list, search, replace):
+    """ Create an empty list tostore the results"""
+    new_list = []
+    """ Iterate through each element in the original list"""    
+    for element in my_list:
+        """ Check if the element is equal to search element"""
+        if element == search:
+            """ If yes, append the replacement element to the new list"""
+            new_list.append(replace)
+        else:
+            """ If false, still append the orginal list to the new new list"""
+            new_list.append(replace)
+    """ Return the new list with replacement """
+    return new_list
+
