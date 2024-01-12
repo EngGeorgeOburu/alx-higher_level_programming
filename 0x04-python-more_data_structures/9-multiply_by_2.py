2e61ba240b4dd828df924f1726db73c5efa6293e
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-12 11:21:52 +0300

    fixing PEP8

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    """ Function that reurn new dictionary with all values x2 """
     mult_dict = {key: value * 2 for key, value in a_dictionary.items()}
     return (mult_dict)
