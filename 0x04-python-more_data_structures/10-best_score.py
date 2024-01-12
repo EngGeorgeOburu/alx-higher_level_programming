5ae9f45f2d9ee84c895435e2f50e453376ad3dca
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-12 11:33:19 +0300

    max dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    """ Key that returns a key woth the biggest interger value """
+    """ Check if the input is a non-empty dictionary """
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None    
+        return None
+    """ Initialize variables to store initial key and max value """
     values = list(a_dictionary.keys())[0]
     max_value = a_dictionary[values]
+    """ Iterate through the dictionary to find key with the max value"""
     for i, j in a_dictionary.items():
         if j > max_value:
             max_value = j
