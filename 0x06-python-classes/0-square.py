bb833675ec8768e080af196174c954defaf171eb
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-23 16:56:59 +0300

    PEP8

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 class Square():
     """ A class representing a square.
-    
-    This class serves as a basic representation of a square without any specific attributes or methods.
-    
+    without any specific attributes or methods.
     Atrributes:
         None
-    
     Methods:
         None
     """
