60cc4e1b838734b3800d293912fd2b2af4da847c
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-23 16:49:54 +0300

    fixing google style

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 class Square():
+    """ A class representing a square.
+    
+    This class serves as a basic representation of a square without any specific attributes or methods.
+    
+    Atrributes:
+        None
+    
+    Methods:
+        None
+    """
     pass
