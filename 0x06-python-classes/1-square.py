8f30259a6750eecd0a8ccd30d50c131d44dbdd29
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-24 11:48:13 +0300

    google docsting stly

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 class Square:
+    """A class representing a square.
+    Attributes:
+        _size (int): The size of the square.
+    """
     def __init__(self, size):
+        """
+        Initialize a new instance of the Square class.
+        Parameters:
+            size (int): The size of the square.
+        """
         self._size = size
         """self._size = None"""
