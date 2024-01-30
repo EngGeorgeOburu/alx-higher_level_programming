461d43e5107fd7cb052e8d6ec843198fd825ba5a
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-30 04:47:11 +0300

    1-rect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.width=0
-        self.height=0
+        self.width = 0
+        self.height = 0
+
     @property
     def width(self):
         return self._width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -13,9 +15,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self._width = value
+
     @property
     def height(self):
         return self._height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
