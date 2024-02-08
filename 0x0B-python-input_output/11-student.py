f88c4be8c01dae3f6a0f5a7bfc4f6fdb874dfdd2
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-02-08 06:09:00 +0300

    inout

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,5 +4,36 @@ This module defines a class 11-student
 """
 
 
-def __inti__(self,first_name, last_name, age):
+class Student:
+    """
+    Class representation of student.
+    """
+
+    def __inti__(self,first_name, last_name, age):
+        """
+        Initializes a new student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self,attrs=None):
+        """
+        This methods gets dictionary representation of the student
+        if the attrs is alist of strings, represents only those attributes
+        inlcuded in the list
+        """
+        if (type(attrs) == list and
+                all(type(element == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Rplaces all attributes of the student.
+        """
+        for i, j in json.items():
+            setatrr(self, i, j)
+
+
 
