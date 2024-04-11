f9730c0182492f28ff3437f2986bfcb812b65426
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-04-11 11:44:16 +0300

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.reverse = function (list) {
+exports.esrever = function (list) {
   return list.reduceRight(function (array, current) {
     array.push(current);
     return array;
