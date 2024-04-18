4b7bb7a4e05d08033b4e01f5165bac8b61a5fb6f
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-04-18 12:28:08 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,18 +15,18 @@ if __name__ == "__msin__":
     states from the database
     """
     db = MySQLdb.connect(
-            host = "localhost",
-            port = 3306,
-            user = sys.argv[1],
-            passwd = sys.argv[2],
-            db = sys.argv[3],
-            charset = 'utf8'
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset='utf8'
             )
     cur = db.cursor()
-    cur.execute ("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
     for row in cur.fetchall():
-       print(row)
+        print(row)
 
     cur.close()
     db.close()
