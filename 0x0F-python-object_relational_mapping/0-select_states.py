6c9451287368a1e13436864904aeba5b3a7b38b4
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-04-18 12:46:12 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -20,8 +20,8 @@ if __name__ == "__msin__":
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
-            charset='utf8'
-            )
+            charset='utf8')
+
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
